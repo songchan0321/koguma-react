939974e57e61c4eddc823d183e457f4ac890a992
chan <dlscks97@gmail.com>
2023-12-15 17:39:39 +0900

    fix : report UI 약간 꾸몄음
    List, Get, Add Form 꾸밈
    
    2023.12.15 / 17:39 / SIC

--- src/component/member/ListReportForm.jsx
+++ src/component/member/ListReportForm.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { authInstance } from "../../apis/utils/instance";
+import { authInstance } from '../../apis/utils/instance';
 import { useNavigate, useParams } from 'react-router-dom';
+import {
+    ThemeProvider,
+    createTheme,
+    CircularProgress,
+    Button,
+    List,
+    ListItem,
+    Typography,
+} from '@mui/material';
 
 const ListReportForm = () => {
     const { id } = useParams();
@@ -12,8 +21,7 @@ const ListReportForm = () => {
         const fetchReportList = async () => {
             try {
                 const response = await authInstance.get('/member/report/list');
-                console.log('Response:', response); // 응답 전체를 출력
-                console.log('Data:', response.data); // 응답에서 데이터만 출력
+
                 setReportList(response.data);
                 setLoading(false);
             } catch (error) {
@@ -34,25 +42,49 @@ const ListReportForm = () => {
         navigate('/member/report/add');
     };
 
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#8a2be2', // 보라색
+            },
+        },
+    });
+
     return (
-        <div>
-            {loading ? (
-                <p>데이터를 불러오는 중입니다...</p>
-            ) : (
-                <ul>
-                    {reportList.map((report) => (
-                        <li key={report.reportTitle}>
-                            {report.reportTitle}
-                            {' '}
-                            <button onClick={() => handleNavigateToReportDetail(report.reportTitle)}>
-                                신고 상세 정보
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            )}
-            <button onClick={handleAddReportButtonClick}>문의 및 신고 추가</button>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div>
+                {loading ? (
+                    <CircularProgress color="primary" />
+                ) : (
+                    <div>
+                        <List>
+                            {reportList.map((report) => (
+                                <ListItem key={report.reportTitle}>
+                                    <Typography variant="body1" color="primary">
+                                        {report.reportTitle}{' '}
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            onClick={() => handleNavigateToReportDetail(report.reportTitle)}
+                                        >
+                                            신고 상세 정보
+                                        </Button>
+                                    </Typography>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+                )}
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleAddReportButtonClick}
+                    style={{ marginTop: '10px' }}
+                >
+                    문의 및 신고 추가
+                </Button>
+            </div>
+        </ThemeProvider>
     );
 };
 
