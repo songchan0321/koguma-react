d15aada0dec9587e33662c4b8f4fbbb26f99da3d
chan <dlscks97@gmail.com>
2023-12-20 12:40:15 +0900

    fix : 프로필 화면 UI 수정
    수정수정
    
    2023.12.20 / 12:40 / SIC

--- src/pages/member/Profile.jsx
+++ src/pages/member/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Grid, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import ProfileForm from "../../component/member/ProfileForm";
 import { authInstance } from "../../apis/utils/instance";
@@ -8,49 +8,97 @@ import BottomBar from "../../component/common/BottomBar";
 const Profile = () => {
   const navigate = useNavigate();
   const [member, setMember] = useState(null);
+  const [profileImageUrl, setProfileImageUrl] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // 서버에서 현재 유저 정보를 가져오는 API 호출
     const fetchMemberData = async () => {
       try {
-        const response = await authInstance.get("/member/profile/get", {
+        // 첫 번째 API 호출
+        const response1 = await authInstance.get("/member/profile/get", {
           headers: {
             Accept: "application/json",
           },
         });
-        setMember(response.data); // 유저 정보를 상태에 저장
+        setMember(response1.data);
+
+        // 두 번째 API 호출
+        const response2 = await authInstance.get("/member/profile/get", {
+          headers: {
+            Accept: "application/json",
+          },
+        });
+        setProfileImageUrl(response2.data);
+        setLoading(false);
       } catch (error) {
         console.error("오류 발생:", error);
+        // 오류 발생 시에도 로딩 상태 갱신
+        setLoading(false);
       }
     };
 
     fetchMemberData();
-  }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 호출
+  }, []);
 
   return (
-    <Box p={3} sx={{ overflowY: "auto", maxHeight: "calc(100vh - 64px)" }}>
-      {/* Box에 overflowY 및 maxHeight 속성 추가 */}
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          {/* 프로필 사진 */}
-          <img
-            src="path/to/profile/image.jpg"
-            alt="Profile"
-            style={{ width: "100%", borderRadius: "50%" }}
-          />
+      <Box p={3} sx={{ overflowY: "auto", maxHeight: "calc(100vh - 64px)" }}>
+        <Grid container spacing={2} justifyContent="flex-end" alignItems="flex-start">
+          {/* 내 정보 수정 버튼 */}
+          <Grid item>
+            <Button
+                onClick={() => navigate("/member/update")}
+                variant="contained"
+                color="secondary"
+                sx={{ width: '100px', height: '40px' }}
+            >
+              내 정보 수정
+            </Button>
+          </Grid>
+
+          {/* 로그아웃 버튼 */}
+          <Grid item>
+            {/* 수정: marginLeft 속성 추가 */}
+            <Button
+                onClick={() => {
+                  localStorage.removeItem("token");
+                  navigate("/common/login");
+                }}
+                variant="contained"
+                color="secondary"
+                sx={{ width: '100px', height: '40px', marginLeft: '2px' }}
+            >
+              로그아웃
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={8}>
-          {/* 닉네임 */}
-          <Typography variant="h4" gutterBottom>
-            {member?.nickname || "닉네임이 없습니다."}
-          </Typography>
-          {/* ProfileForm 컴포넌트 추가 */}
-          <ProfileForm navigate={navigate} />
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            {/* 로딩 중일 때 CircularProgress 표시 */}
+            {loading ? (
+                <CircularProgress />
+            ) : (
+                // 로딩이 완료되면 멤버 이미지 표시
+                member && <img src={member.profileURL} alt="Profile" style={{ width: "20%", borderRadius: "20%" }} />
+            )}
+          </Grid>
+          <Grid item xs={12} md={8}>
+            {/* 닉네임 */}
+            <Typography variant="h4" gutterBottom>
+              {/* 로딩 중일 때 CircularProgress 표시 */}
+              {loading ? (
+                  <CircularProgress size={20} />
+              ) : (
+                  // 로딩이 완료되면 멤버 닉네임 표시
+                  member?.nickname
+              )}
+            </Typography>
+            {/* ProfileForm 컴포넌트 추가 */}
+            <ProfileForm navigate={navigate} />
+          </Grid>
+          {/* ... 나머지 컴포넌트들 추가 */}
         </Grid>
-        {/* ... 나머지 컴포넌트들 추가 */}
-      </Grid>
-      <BottomBar />
-    </Box>
+        <BottomBar />
+      </Box>
   );
 };
 
