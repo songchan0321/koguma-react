3f334aef3674cd37964e3a650263689fa02bd4a7
chan <dlscks97@gmail.com>
2023-12-21 20:49:42 +0900

    feat : 프로필 사진 둥글게, 경계선 추가
    둥글게, 경계선
    
    2023.12.21 / 20:49 / SIC

--- src/pages/member/Profile.jsx
+++ src/pages/member/Profile.jsx
@@ -88,7 +88,7 @@ const Profile = () => {
             ) : (
                 // 로딩이 완료되면 멤버 이미지 표시
                 member && member.profileURL ? (
-                    <img src={member.profileURL} alt="" style={{ width: "40%", marginLeft: '103px', marginTop: '30px', borderRadius: '50%' }} />
+                    <img src={member.profileURL} alt="" style={{ width: "40%", marginLeft: '103px', marginTop: '30px', borderRadius: '40%' }} />
                 ) : (
                     // 이미지가 없을 때 아이콘 등의 대체 컨텐츠를 표시
                     <div></div>
