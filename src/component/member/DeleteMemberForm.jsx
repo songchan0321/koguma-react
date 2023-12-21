7cf078b4e53e02636535d4a014a4fbdcc6762db7
chan <dlscks97@gmail.com>
2023-12-22 02:53:34 +0900

    feat : 휴대폰 인증 부활(주석은 쳐둠), 회원 탈퇴 휴대폰 인증 추가
    로직 추가
    
    2023.12.12 / 02:53 / SIC

--- src/component/member/DeleteMemberForm.jsx
+++ src/component/member/DeleteMemberForm.jsx
@@ -13,13 +13,13 @@ const DeleteMemberForm = ({ onSubmit }) => {
 
     const handleSendConfirmation = async () => {
         try {
-            // TODO: 서버에 휴대폰 번호로 인증 코드 요청하는 API 호출
-            // const response = await sendConfirmationCode(phone);
-            // 실제로는 서버와의 통신이 필요하며, 휴대폰 번호로 인증 코드를 요청하고 응답을 기다리는 로직이 들어갑니다.
+            const response = await authInstance.post("/auth/sendSms", {
+                to: phone,
+            });
 
-            // 여기서는 간단한 시뮬레이션으로 확인 메시지를 띄우고, 사용자가 확인을 누르면 인증이 완료된 것으로 가정합니다.
-            const confirmed = window.confirm("휴대폰 번호로 전송된 인증 코드를 확인했습니까?");
-            if (confirmed) {
+            // 인증번호가 성공적으로 전송되었다면 화면 전환을 막음
+            if (response.status === 200) {
+                window.alert('인증번호가 발송되었습니다.');
                 setIsConfirmationSent(true);
             }
         } catch (error) {
@@ -29,20 +29,26 @@ const DeleteMemberForm = ({ onSubmit }) => {
 
     const handleDeleteMember = async () => {
         try {
-            // TODO: 서버에 회원 탈퇴 요청하는 API 호출
-            const confirmed = window.confirm("정말로 회원 탈퇴하시겠습니까?");
-            if (confirmed) {
-                const response = await authInstance.put("/member/delete", { phone, confirmationCode });
+            if (!isConfirmationSent) {
+                // 휴대폰 인증을 완료하지 않은 경우 알림 창 표시
+                window.alert('휴대폰 인증을 완료해야 합니다.');
+                return;
+            }
 
-                // 실제로는 서버 응답에 따른 로직을 추가해야 합니다.
-                if (response.status === 200) {
-                    // 성공 시 부모 컴포넌트에서 전달받은 onSubmit 함수 호출
-                    await onSubmit();
-                } else {
-                    // 실패 시 에러 처리
-                    console.error("회원 탈퇴 실패:", response.statusText);
-                    window.alert("회원 탈퇴에 실패했습니다. 다시 시도해주세요.");
-                }
+            // TODO: 서버에 회원 탈퇴 요청하는 API 호출
+            const response = await authInstance.post("/auth/verifySms", {
+                to: phone,
+                authNum: confirmationCode,
+            });
+
+            // 실제로는 서버 응답에 따른 로직을 추가해야 합니다.
+            if (response.status === 200) {
+                // 성공 시 부모 컴포넌트에서 전달받은 onSubmit 함수 호출
+                await onSubmit();
+            } else {
+                // 실패 시 에러 처리
+                console.error("회원 탈퇴 실패:", response.statusText);
+                window.alert("회원 탈퇴에 실패했습니다. 다시 시도해주세요.");
             }
         } catch (error) {
             console.error("오류 발생:", error);
@@ -83,7 +89,6 @@ const DeleteMemberForm = ({ onSubmit }) => {
                             fullWidth
                             value={confirmationCode}
                             onChange={(e) => setConfirmationCode(e.target.value)}
-
                         />
                     </Grid>
                     <Grid item xs={12}>
