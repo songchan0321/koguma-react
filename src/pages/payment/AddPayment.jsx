53e3efe96d8b1033f014644ce40e0fe3ade4ce2f
git-kth <hisidblack@kakao.com>
2023-12-26 14:39:54 +0900

    AddPayment Back 추가

--- src/pages/payment/AddPayment.jsx
+++ src/pages/payment/AddPayment.jsx
@@ -2,10 +2,12 @@ import { Container } from "@mui/material";
 import PaymentForm from "../../component/payment/PaymentForm";
 import TopBar from "../../component/payment/TopBar";
 import MarginEmpty from "../../component/payment/MarginEmpty";
+import Back from "../../component/common/Back";
 
 const AddPayment = () => {
   return (
     <Container fixed>
+      <Back />
       <TopBar>고구마 페이 생성</TopBar>
       <MarginEmpty value={"65px"} />
       <PaymentForm />
